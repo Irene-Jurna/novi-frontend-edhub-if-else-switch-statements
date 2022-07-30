5f0263eb0355b93f502612c19fd29bfb11dbc175
Irene Jurna <irene.jurna@gmail.com>
2022-07-30 08:55:47 +0200

    oefenopdracht EdHub H2.8

--- conditionals.js
+++ conditionals.js
@@ -9,16 +9,22 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 // 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op        // geeft false wanneer gelogd
 // ==========================================
 
+const compareNumber1 = 10 > 20;
+console.log(compareNumber1);
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op        // geeft true wanneer gelogd
 // ==========================================
 
+const compareNumber2 = 10 !== 11;
+console.log(compareNumber2);
 
 // ==========================================
 // 3. Declareer een variabele en sla daar in op of de woorden 'zoet' en 'sappig' hetzelfde zijn                // geeft false wanneer gelogd
 // ==========================================
 
+const compareWord1 = "zoet" === "sappig";
+console.log(compareWord1);
 
 // ==========================================
 // 4a. Declareer een variabele en sla daar het rapportcijfer 9 in op.
@@ -26,6 +32,10 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 //     Als dat zo is, loggen we 'Cum laude!' in de console.                                                     // geeft 'Cum laude!' wanneer gelogd
 // ==========================================
 
+const grade = 9;
+if (grade > 8) {
+    console.log('Cum Laude!');
+}
 
 // ==========================================
 // 5a. Gebruik onderstaande variabelen isRaining en temperature;
@@ -35,4 +45,10 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 // ==========================================
 
 const isRaining = false;
-const temperature = 23;
+const temperature = 11;
+
+if (temperature >= 21 && !isRaining) {
+    console.log("Je kunt een korte broek aan!");
+} else if (temperature < 12 || isRaining) {
+    console.log("Doe een jas aan!")
+}
